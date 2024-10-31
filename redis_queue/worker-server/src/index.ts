e1639573b11b9b52428afdeba505802902663f70
pabitra <89703515+appu900@users.noreply.github.com>
2024-10-31 18:42:44 +0530

    chnages a few line code

--- redis_queue/worker-server/src/index.ts
+++ redis_queue/worker-server/src/index.ts
@@ -5,7 +5,7 @@ async function handleWorker() {
   await client.connect();
   console.log("Connected to Redis");
   while (1) {
-    const response = await client.rPop("submissions");
+    const response = await client.brPop("submissions", 0);
     response && console.log(response);
     // await new Promise((resolve) => setTimeout(resolve, 1000));
     // console.log("Processed User Submission")
